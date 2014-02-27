f8230854a2b77fe346bc1d97a05d526da388f2a1
joshua <joshua@Joshua-PC>
2014-02-27 09:46:47 -0300

    Terminadas las clases Rubro y SubRubro. Listas para ser entidades mapeables.

--- src/datos/Rubro.java
+++ src/datos/Rubro.java
@@ -6,10 +6,60 @@
 
 package datos;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author joshua
  */
 class Rubro {
+    private int idRubro;
+    private String nombre;
+    
+    private Set<Articulo> articulos = new TreeSet<Articulo>();
+    private Set<Proveedor> proveedores = new TreeSet<Proveedor>();
+
+    public Rubro() {
+    }
+
+    public Rubro(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdRubro() {
+        return idRubro;
+    }
+
+    private void setIdRubro(int idRubro) {
+        this.idRubro = idRubro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(Set<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+
+    public Set<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(Set<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+    
+    
     
 }
+
--- src/datos/SubRubro.java
+++ src/datos/SubRubro.java
@@ -6,10 +6,99 @@
 
 package datos;
 
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author joshua
  */
 class SubRubro {
+    private int idSubRubro;
+    private String nombre;
+    private double precioCompra;
+    private double precioVenta;
+    private Rubro rubro;
+    private GregorianCalendar fecUltimaModif;
+    private double ultimoPorc;
+    private Set<Articulo> articulos = new TreeSet<Articulo>();
+
+    public SubRubro() {
+    }
+
+    public SubRubro(String nombre, double precioCompra, double precioVenta, Rubro rubro, GregorianCalendar fecUltimaModif, double ultimoPorc) {
+        this.nombre = nombre;
+        this.precioCompra = precioCompra;
+        this.precioVenta = precioVenta;
+        this.rubro = rubro;
+        this.fecUltimaModif = fecUltimaModif;
+        this.ultimoPorc = ultimoPorc;
+    }
+
+    public int getIdSubRubro() {
+        return idSubRubro;
+    }
+
+    private void setIdSubRubro(int idSubRubro) {
+        this.idSubRubro = idSubRubro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Rubro getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Rubro rubro) {
+        this.rubro = rubro;
+    }
+
+    public GregorianCalendar getFecUltimaModif() {
+        return fecUltimaModif;
+    }
+
+    public void setFecUltimaModif(GregorianCalendar fecUltimaModif) {
+        this.fecUltimaModif = fecUltimaModif;
+    }
+
+    public double getUltimoPorc() {
+        return ultimoPorc;
+    }
+
+    public void setUltimoPorc(double ultimoPorc) {
+        this.ultimoPorc = ultimoPorc;
+    }
+
+    public Set<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(Set<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+    
     
 }
